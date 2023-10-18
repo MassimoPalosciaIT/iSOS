84c48ff2233aeada4e6867a3ecdeea7e584b431f
Matt Novoselov <59065228+matt-novoselov@users.noreply.github.com>
2023-10-18 20:27:25 +0200

    Re-uploded files because hierarchy was broken

---
+++ iSOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSOS
+//
+//  Created by Massimo Paloscia on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Massimo!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSOS/iSOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSOSApp.swift
+//  iSOS
+//
+//  Created by Massimo Paloscia on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iSOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

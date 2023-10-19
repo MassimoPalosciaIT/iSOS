7c6fc238a3a116e51a42936f7798e53581d9c245
khadija <aitelfqih.khadija@gmail.com>
2023-10-19 11:14:39 +0200

    uppdate text

--- iSOS/ContentView.swift
+++ iSOS/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Matt")
+            Text("Hello, One Piece")
             Text("Ciao")
         }
         .padding()

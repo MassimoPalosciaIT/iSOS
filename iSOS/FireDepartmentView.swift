8bf8d87a6e6dfc3e34f59b3d78cead1defba4324
MassimoPalosciaIT <148210878+MassimoPalosciaIT@users.noreply.github.com>
2023-10-20 12:42:03 +0200

    feature: creation of emergency pages, and linking with main

---
+++ iSOS/FireDepartmentView.swift
@@ -0,0 +1,18 @@
+//
+//  FireDepartmentView.swift
+//  iSOS
+//
+//  Created by Massimo Paloscia on 20/10/23.
+//
+
+import SwiftUI
+
+struct FireDepartmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FireDepartmentView()
+}
---
+++ iSOS/MedicalEmergencyView.swift
@@ -0,0 +1,18 @@
+//
+//  MedicalEmergencyView.swift
+//  iSOS
+//
+//  Created by Massimo Paloscia on 20/10/23.
+//
+
+import SwiftUI
+
+struct MedicalEmergencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicalEmergencyView()
+}

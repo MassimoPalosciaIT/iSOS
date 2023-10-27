f4a8efa29babb130c6328536376d1a35da9e450e
MassimoPalosciaIT <148210878+MassimoPalosciaIT@users.noreply.github.com>
2023-10-27 11:51:50 +0200

    + CVS file to project + button to first aid view

--- iSOS/View/Sheets/FirstAidView.swift
+++ iSOS/View/Sheets/FirstAidView.swift
@@ -17,7 +17,10 @@ struct FirstAidView: View {
                 FirstAidSelectionButton(iconName: "bolt.heart", title: "Heart Attack", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
                 FirstAidSelectionButton(iconName: "brain.head.profile", title: "Stroke", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
                 FirstAidSelectionButton(iconName: "bandage", title: "Fracture", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
-                FirstAidSelectionButton(iconName: "flame", title: "Fracture", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
+                FirstAidSelectionButton(iconName: "rays", title: "Asthma Attack", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
+                FirstAidSelectionButton(iconName: "flame", title: "Diabetic Emegerncy", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
+                FirstAidSelectionButton(iconName: "flame", title: "Epileptic seizure", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
+                FirstAidSelectionButton(iconName: "flame", title: "Allergic reaction", gradientColor1: .redGradient1, gradientColor2: .redGradient2)
             }
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)

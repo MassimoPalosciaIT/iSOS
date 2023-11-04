ac527b4d396a631d52616e62a44800196f292053
Matt Novoselov <matveynovoselov@gmail.com>
2023-11-04 21:00:47 +0100

    Rewrote first aid tips parsing

--- iSOS/Models/FirstAidModel.swift
+++ iSOS/Models/FirstAidModel.swift
@@ -6,37 +6,6 @@ struct FirstAidModel: Identifiable {
     var title: String
     var iconName: String
 }
-//
-//func getFirstAidRows(for firstAidName: String) -> [String] {
-//    guard let path = Bundle.main.path(forResource: "firstAid", ofType: "csv") else {
-//        return ["Error: Couldn't find the CSV file"]
-//    }
-//    
-//    do {
-//        let csvData = try String(contentsOfFile: path)
-//        let rows = csvData.components(separatedBy: "\n")
-//        
-//        var results: [String] = []
-//        
-//        for row in rows {
-//            let columns = row.components(separatedBy: ";")
-//            if columns.first == firstAidName {
-//                results = row.split(separator: ";").map(String.init)
-//                break
-//            }
-//        }
-//        
-//        results = results.map { $0.replacingOccurrences(of: "\r", with: "") }
-//        results = results.filter { !$0.isEmpty }
-//        if !results.isEmpty {
-//            results.removeFirst()
-//        }
-//        return results
-//        
-//    } catch {
-//        return ["Error: Couldn't read the CSV file"]
-//    }
-//}
 
 func getFirstAidRows(for firstAidName: String) -> [String] {
     guard let csvPath = Bundle.main.path(forResource: "firstAid", ofType: "csv"),

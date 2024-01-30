f597b7d42b6cce5d199f86be7bc79be0d55c294d
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-30 14:58:42 +0100

    Offload unnecessary files and content

--- iSOS/iSOSApp.swift
+++ iSOS/iSOSApp.swift
@@ -2,10 +2,27 @@ import SwiftUI
 
 @main
 struct iSOSApp: App {
+    @StateObject var locationViewModel = LocationViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            Bootstrap()
-                .preferredColorScheme(.dark)
+            Group{
+                switch locationViewModel.authorizationStatus {
+                case .notDetermined:
+                    AnyView(RequestLocationView())
+                        .environmentObject(locationViewModel)
+                case .restricted:
+                    ErrorView(errorText: "Location use is restricted")
+                case .denied:
+                    ErrorView(errorText: "The app does not have location permissions")
+                case .authorizedAlways, .authorizedWhenInUse:
+                    SelectionView()
+                default:
+                    Text("Unexpected status. Contact support")
+                }
+            }
+            .environmentObject(locationViewModel)
+            .preferredColorScheme(.dark)
         }
     }
 }

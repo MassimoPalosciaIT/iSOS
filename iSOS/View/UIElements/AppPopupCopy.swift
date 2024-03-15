d8d5594f2033503d5c7dd8ad7fc5f79c3613e409
Matt Novoselov <matveynovoselov@gmail.com>
2024-03-15 10:52:50 +0100

    Code structure refactoring + moved source files to "Source" folder

---
+++ iSOS/View/UIElements/AppPopupCopy.swift
@@ -0,0 +1,18 @@
+//
+//  AppPopupCopy.swift
+//  iSOS
+//
+//  Created by Matt Novoselov on 15/03/24.
+//
+
+import SwiftUI
+
+struct AppPopupCopy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppPopupCopy()
+}
---
+++ iSOS/View/UIElements/EmergencySelectionButton.swift
@@ -0,0 +1,18 @@
+//
+//  EmergencySelectionButton.swift
+//  iSOS
+//
+//  Created by Matt Novoselov on 15/03/24.
+//
+
+import SwiftUI
+
+struct EmergencySelectionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmergencySelectionButton()
+}

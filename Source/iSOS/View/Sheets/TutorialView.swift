c0d1fbaaff92d8aaf308da2eb3eae155b63d3433
Matt Novoselov <matveynovoselov@gmail.com>
2024-04-16 17:22:31 +0200

    Added aniamtion to tutorial view

--- Source/iSOS/View/Sheets/TutorialView.swift
+++ Source/iSOS/View/Sheets/TutorialView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TutorialView: View {
     
     @AppStorage("isOnboarding") var isOnboarding: Bool?
+    @State var animationPhase: Int = 0
     
     var body: some View {
         
@@ -20,20 +21,25 @@ struct TutorialView: View {
                     .clipShape(.rect(cornerRadius: proxy.size.width * 2/9))
             }
             .frame(width: 80, height: 80)
+            .opacity(animationPhase >= 1 ? 1 : 0)
             
             Text("Welcome to Yaha")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .multilineTextAlignment(.center)
+                .opacity(animationPhase >= 1 ? 1 : 0)
             
             VStack(spacing: 20){
                 TutorialElement(iconName: "phone.circle", title: "Emergency number locator", description: "The app determines your country's emergency number.")
+                    .opacity(animationPhase >= 2 ? 1 : 0)
                 
                 TutorialElement(iconName: "doc.on.doc", title: "Copy coordinates", description: "Click on the coordinates to copy them to the clipboard.")
+                    .opacity(animationPhase >= 3 ? 1 : 0)
                 
                 TutorialElement(iconName: "square.and.arrow.down", title: "Download offline maps", description: "Make sure to download offline maps before your journey.")
+                    .opacity(animationPhase >= 4 ? 1 : 0)
             }
-            .padding(.vertical)
+            .padding(.all)
             
             Spacer()
             
@@ -45,9 +51,21 @@ struct TutorialView: View {
             .buttonStyle(BorderedProminentButtonStyle())
             .controlSize(.large)
             .tint(.redGradient1)
+            .opacity(animationPhase >= 5 ? 1 : 0)
         }
         .padding()
         .padding(.top)
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 0.8, repeats: true) { timer in
+                if animationPhase < 5 {
+                    withAnimation {
+                        animationPhase += 1
+                    }
+                } else {
+                    timer.invalidate()
+                }
+            }
+        }
         
     }
 }
